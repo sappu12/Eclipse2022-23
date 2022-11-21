41cbc40bd2916bb010e234b8c83a7b6aab8386ea
sappu12 <swapna.guduru12@gmail.com>
2022-11-21 15:54:32 -0500

    added second comment

--- Demo/src/mydemopackage/HelloWorld.java
+++ Demo/src/mydemopackage/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is first hello");
+		System.out.println("Second comment");
 	}
 
 }
