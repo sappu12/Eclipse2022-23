56719abef648aca0545ebc89787e2ece2b9db8f9
sappu12 <swapna.guduru12@gmail.com>
2022-11-21 15:46:59 -0500

    made another file

---
+++ Demo/src/mydemopackage/Demo2.java
@@ -0,0 +1,10 @@
+package mydemopackage;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is second hello");
+	}
+
+}
